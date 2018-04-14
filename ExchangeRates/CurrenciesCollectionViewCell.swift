317bb676eda6291fce92638f1728b18716d58e2a
Tanya Tomchuk <tanyem@protonmail.com>
2018-04-14 20:11:06 +0300

    first commit

---
+++ ExchangeRates/CurrenciesCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CurrenciesCollectionViewCell.swift
+//  ExchangeRates
+//
+//  Created by Tanya Tomchuk on 14/04/2018.
+//  Copyright © 2018 Tanya Tomchuk. All rights reserved.
+//
+
+import UIKit
+
+class CurrenciesCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
